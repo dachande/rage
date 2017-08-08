b4c60caba0f45bf8471d8c3d88c3a9c7be56118a
Daniel Schultheis <d.schultheis@reizwerk.com>
2017-08-08 12:56:52 +0200

    Replaced bootstrap video modal with magnific popup

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,28 +5,9 @@ var slick = require('slick-carousel')
 var magnificPopup = require('magnific-popup')
 
 $(document).ready(function () {
-  var $videoModal = $('.Video__Modal')
-
-  $videoModal
-    .each(function (e) {
-      var $modal = $(this)
-      var $iframe = $modal.find('iframe')
-      var iframeSrc = $iframe.attr('src')
-
-      $modal.data('src', iframeSrc)
-      $iframe.attr('src', '')
-    })
-
-    .on('show.bs.modal', function (e) {
-      $modal = $(this)
-      $modal.find('iframe').attr('src', $modal.data('src'))
-    })
-
-    .on('hidden.bs.modal', function (e) {
-      $modal = $(this)
-      $modal.find('iframe').attr('src', '')
-    })
-
+  /**
+   * Initialize slideshow(s) with slick carousel
+   */
   $('.Slideshow').slick({
     dots: false,
     arrows: true,
@@ -37,10 +18,13 @@ $(document).ready(function () {
     focusOnSelect: true
   })
 
+  /**
+   * Slideshow images will open in a magnific popup layer
+   */
   $('.Slideshow').magnificPopup({
     delegate: 'a',
     type: 'image',
-    mainClass: 'mfp-with-zoom mfp-fade',
+    mainClass: 'mfp-with-zoom',
 
     zoom: {
       enabled: true,
@@ -54,5 +38,29 @@ $(document).ready(function () {
     gallery: {
       enabled: true
     }
+  })
+
+  /**
+   * Images with Image__Link--Popup class will open in magnific popup layer
+   */
+  $('.Image__Link--Popup').magnificPopup({
+    type: 'image',
+    mainClass: 'mfp-with-zoom',
+
+    zoom: {
+      enabled: true,
+      duration: 300,
+      easing: 'ease-in-out',
+      opener: function (openerElement) {
+        return openerElement.is('img') ? openerElement : openerElement.find('img')
+      }
+    }
+  })
+
+  /**
+   * Videos will open in a magnific popup layer
+   */
+  $('.Video__Link').magnificPopup({
+    type: 'iframe'
   })
 })
